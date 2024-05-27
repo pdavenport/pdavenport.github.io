35dd6b1cc93a3dec484b22dc6be5c42d542d713a
pdavenport <peterkdavenport@gmail.com>
2024-05-27 14:20:48 -0400

    css

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,17 @@
+"use client";
+import { CssSineWave } from "@/components/cssSineWave";
 import { TextScroller } from "@/components/textScroller";
+
 export default function About() {
   return (
     <>
       something something about me
-      <TextScroller />
+      <div className="absolute z-20">
+        <TextScroller />
+      </div>
+      <div className="absolute z-10">
+        <CssSineWave />
+      </div>
     </>
   );
 }
